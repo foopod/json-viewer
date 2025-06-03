218e2bef11443537a7447690d7d02fd1f16572d3
Jono Shields <jonathonshields@gmail.com>
2025-06-03 22:12:31 +1200

    move example data

--- src/App.tsx
+++ src/App.tsx
@@ -18,30 +18,30 @@ function App() {
   const [searchResults, setSearchResults] = useState<string[][]>([])
   const [searchResultIndex, setSearchResultsIndex] = useState(0)
 
-  useEffect(() => {
-    const exampleData = {
-      glossary: {
-        title: "example glossary",
-        GlossDiv: {
-          title: "S",
-          GlossList: {
-            GlossEntry: {
-              ID: "SGML",
-              SortAs: "SGML",
-              GlossTerm: "Standard Generalized Markup Language",
-              Acronym: "SGML",
-              Abbrev: "ISO 8879:1986",
-              GlossDef: {
-                para: "A meta-markup language, used to create markup languages such as DocBook.",
-                GlossSeeAlso: ["GML", "XML"]
-              },
-              GlossSee: "markup"
-            }
+  const exampleData = {
+    glossary: {
+      title: "example glossary",
+      GlossDiv: {
+        title: "S",
+        GlossList: {
+          GlossEntry: {
+            ID: "SGML",
+            SortAs: "SGML",
+            GlossTerm: "Standard Generalized Markup Language",
+            Acronym: "SGML",
+            Abbrev: "ISO 8879:1986",
+            GlossDef: {
+              para: "A meta-markup language, used to create markup languages such as DocBook.",
+              GlossSeeAlso: ["GML", "XML"]
+            },
+            GlossSee: "markup"
           }
         }
       }
     }
+  }
 
+  useEffect(() => {
     setData(exampleData)
   }, [])
 
